f2d0f145caa973d12fbc3c49aed7906a54ebd0d4
ChuGui <ChuGui>
2019-02-02 13:37:20 +0100

    Installation de webpack encore pour la gestion des assest, Installation Jquery/bootstrap

--- assets/js/base.js
+++ assets/js/base.js
@@ -6,9 +6,13 @@
  */
 
 // any CSS you require will output into a single css file (app.css in this case)
-require('../css/app.css');
+import '../css/base.scss';
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
-// const $ = require('jquery');
+import $ from 'jquery';
+
+$(document).ready(function(){
+    $('body').append("<h1>Hello</h1>");
+});
 
 console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
